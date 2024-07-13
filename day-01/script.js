b139f452f0c98c133cf225d70b23c3a7e561f016
Ritesh Kumar <146605213+riteshkumaar@users.noreply.github.com>
2024-07-13 13:56:32 +0530

    day 01 done.

---
+++ day-01/script.js
@@ -0,0 +1,63 @@
+// Activity-1 : Variable declaration
+var nums = 31
+console.log(nums);
+
+let myName = "Ritesh"
+console.log(`My name is ${myName}`);
+
+// Activity-2 : Constant Declaration
+const randomValue = false
+console.log(randomValue);
+// randomValue = true // gives error while running the code
+
+// Activity-3 : Data Types
+let num = 100
+console.log(num);
+console.log(typeof num);
+console.log();
+
+let myTeacher ="Hitesh Sir"
+console.log(myTeacher);
+console.log(typeof myTeacher);
+console.log();
+
+let isLoggedIn = true
+console.log(isLoggedIn);
+console.log(typeof isLoggedIn);
+console.log();
+
+let myFriend = {
+    "name": "Human",
+    age: 20,
+    Year: 4,
+    "location": "Deoghar",
+}
+console.log(myFriend);
+console.log(typeof myFriend);
+console.log(typeof myFriend.name);
+console.log(typeof myFriend.age);
+console.log(typeof myFriend.Year);
+console.log(typeof myFriend.location);
+console.log();
+
+let myArr = [1 , 'hey' , 5 , 7 , 9]
+console.log(myArr);
+console.log(typeof myArr);
+console.log(typeof myArr[0]);
+console.log(typeof myArr[1]);
+
+// Activity-4 : Reassigning Variables
+let Value = 23
+console.log("Reassignment using let ");
+console.log(`Initial value : ${Value}`);
+Value = 25
+console.log(`Final value : ${Value}`);
+
+// Activity - 5 : Understanding const
+const val = 23
+console.log("Reassignment using let ");
+console.log(`Initial value : ${val}`);
+val = 25
+console.log(`Final value : ${val}`);
+
+
