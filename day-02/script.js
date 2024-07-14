97ebde24be82a91c68251260c4dd15ba0aeee7ea
Ritesh Kumar <146605213+riteshkumaar@users.noreply.github.com>
2024-07-14 07:25:09 +0530

    Create script.js for day 2

---
+++ day-02/script.js
@@ -0,0 +1,41 @@
+// Activity 1: Arithematic operations
+let a = 15
+let b = 7
+console.log("Sum is "+(a+b));
+console.log("Difference is "+(a-b));
+console.log("Product is "+(a*b));
+console.log("Quotient is "+(a/b));
+console.log("Remainder is "+(a%b));
+
+//Activity -2 : Assignment Operators
+let sum = 10;
+sum += a;
+console.log(sum);
+
+let diff = 15
+diff -= a
+console.log(diff);
+
+// Activity-3 : Comparison Operators
+console.log(`On comaring if ${a} is greater than ${b} we get ${a>b}`)
+console.log(`On comaring if ${a} is smaller than ${b} we get ${a<b}`)
+console.log(10<=10);
+console.log(15<=10);
+console.log(15>=10);
+let ab = '10'
+console.log(ab==10); // == doesn't compare the data type, only compares the value
+console.log(ab===10); // === checks data type as well
+
+// Activity -4 : Logical Operators
+console.log((5==7) && (10>=5));
+console.log((5==7) || (10>=5));
+console.log(!(5==7));
+
+// Activity - 5 : Ternary Operators
+let num = 260;
+(num % 2==0) ? console.log("Even") : console.log("Odd");
+
+
+
+
+
